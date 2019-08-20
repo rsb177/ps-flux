17f7e3299f08748818c1a267e6374ddb6a985c6e
Rich Barlow <rbarlow@encoda.com>
2019-08-20 11:49:34 -0400

    Module 9 (forms) completed

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
+import React from "react";
 import HomePage from "./HomePage";
 import AboutPage from "./AboutPage";
 import Header from "./common/header";
 import CoursesPage from "./CoursesPage";
-import { Route, Switch, Redirect } from 'react-router-dom';
+import { Route, Switch, Redirect } from "react-router-dom";
 import NotFoundPage from "./NotFoundPage";
 import ManageCoursePage from "./ManageCoursePage";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-function App(){
-    return(<div className="container-fluid">
-        <Header />
-        <Switch>
-            <Route path={"/"} exact component={HomePage} />
-            <Route path={"/courses"} component={CoursesPage} />
-            <Route path={"/about"} component={AboutPage} />
-            <Route path={"/course/:slug"} component={ManageCoursePage} />
-            <Route path={"/course"} component={ManageCoursePage} />
-            <Redirect from="/about-page" to={"/about"} />
-            <Route component={NotFoundPage} />
-        </Switch>
+function App() {
+  return (
+    <div className="container-fluid">
+      <Header />
+      <ToastContainer autoClose={3000} hideProgressBar />
+      <Switch>
+        <Route path={"/"} exact component={HomePage} />
+        <Route path={"/courses"} component={CoursesPage} />
+        <Route path={"/about"} component={AboutPage} />
+        <Route path={"/course/:slug"} component={ManageCoursePage} />
+        <Route path={"/course"} component={ManageCoursePage} />
+        <Redirect from="/about-page" to={"/about"} />
+        <Route component={NotFoundPage} />
+      </Switch>
     </div>
-    );
+  );
 }
 
 export default App;
