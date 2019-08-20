d027fb67e5fd4ca2cc9de2289980a555010d4d69
Rich Barlow <rbarlow@encoda.com>
2019-08-20 08:58:36 -0400

    Initial Commit - Mostly through module 9

---
+++ src/Components/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function HomePage() {
+    return (
+        <div className="jumbotron">
+            <h1>Pluralsight</h1>
+            <p>React, Flux and React Router for ultra-reponsive web apps.</p>
+            <Link to="about" className={"btn btn-primary"}>About</Link>
+        </div>
+    );
+}
+
+export default HomePage;
---
+++ src/Components/ManageCoursePage.js
@@ -0,0 +1,37 @@
+import React, {useState} from "react";
+import CourseForm from "./CourseForm";
+import * as courseApi from "../api/courseApi"
+
+
+const ManageCoursePage = props => {
+    const [course, setCourse] = useState({
+        id: null,
+        slug: "",
+        title: "",
+        authorId: null,
+        category: ""
+    });
+
+    function handleChange({target}) {
+        setCourse({
+            ...course,
+            [target.name]: target.value
+        });
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        courseApi.saveCourse(course).then( () => {
+            props.history.push("/courses");
+        });
+    }
+
+    return (
+        <>
+            <h2>Manage Course</h2>
+            <CourseForm course={course} onChange={handleChange} onSubmit={handleSubmit}/>
+        </>
+    );
+};
+
+export default ManageCoursePage;
