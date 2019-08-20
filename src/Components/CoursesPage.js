1395dce4a6cb3700d9d8c5fd2c0bc8c2f9fa8c07
Rich Barlow <rbarlow@encoda.com>
2019-08-20 16:59:10 -0400

    Halfway through adding in Flux

--- src/Components/CoursesPage.js
+++ src/Components/CoursesPage.js
@@ -1,24 +1,31 @@
-import React, {useState, useEffect} from 'react';
-import {getCourses} from "../api/courseApi";
+import React, { useState, useEffect } from "react";
+import courseStore from "../stores/courseStore";
 import CourseList from "./CourseList";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import { loadCourses } from "../actions/courseActions";
 
 function CoursesPage() {
-    const [courses, setCourses] = useState([]);
+  const [courses, setCourses] = useState(courseStore.getCourses());
 
-    useEffect(() => {
-        getCourses().then(_courses => setCourses(_courses));
-    }, []);
+  useEffect(() => {
+    courseStore.addChangeListener(onChange);
+    if (courseStore.getCourses().length === 0) loadCourses();
+    return () => courseStore.removeChangeListener(onChange); //cleanup on unmount
+  }, []);
 
-    return (
-        <>
-            <h2>Courses</h2>
-            <Link className={"btn btn-primary"} to={"/course"}>
-                Add Course
-            </Link>
-            <CourseList courses={courses}/>
-        </>
-    );
+  function onChange() {
+    setCourses(courseStore.getCourses());
+  }
+
+  return (
+    <>
+      <h2>Courses</h2>
+      <Link className={"btn btn-primary"} to={"/course"}>
+        Add Course
+      </Link>
+      <CourseList courses={courses} />
+    </>
+  );
 }
 
 export default CoursesPage;
